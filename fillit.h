7a95526a0d1d81a8baba8e2dbcb77e14c5ab8980
Hugo EYNARD <heynard@e2r3p8.42.fr>
2017-01-07 17:46:14 +0100

    header

---
+++ fillit.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: heynard <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/07 17:24:58 by heynard           #+#    #+#             */
+/*   Updated: 2017/01/07 17:45:48 by heynard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLIT_H
+# define FILLIT_H
+
+# include "libft/includes"
+
+typedef struct	s_env
+{
+	unsigned char	size;
+	short int		*tab;
+}				t_env;
+
+//fonction parsing appele par le main
+t_env		*read_file(char *file_name);
+
+//fonction erreur appelant exit
+void		error();
+
+#endif
